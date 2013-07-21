c77cf886c632d7811605976325dffa0c35d2b5bc
Antonio C Nalesso Moreira <acnalesso@yahoo.co.uk>
2013-07-21 01:25:38 +0100

    watchr over autotest, as autotest was very boring...

--- tests/spec/impressionist_spec.rb
+++ tests/spec/impressionist_spec.rb
@@ -3,28 +3,34 @@ require 'impressionist.rb'
 
 describe Impressionist do
 
-  describe "In order to setup config" do
+  # shortcut
+  let(:imp) { Impressionist }
+
+  describe "Setup config" do
     it "must respond_to #setup" do
-      assert_respond_to Impressionist, :setup
+      assert_respond_to imp, :setup
     end
 
-    it "must returns self(Impressionist)" do
-      Impressionist.setup { |c|
-        assert_equal c, Impressionist
+    it "must returns self(imp)" do
+      imp.setup { |c|
+        assert_equal c, imp
       }
     end
 
     it "must respond to #orm puts 2" do
-      assert_respond_to Impressionist, :orm
+      assert_respond_to imp, :orm
     end
 
     it ":active_record must be default value" do
-      assert_equal Impressionist.orm, :active_record
+      assert_equal imp.orm, :active_record
     end
 
     it "must change default of orm" do
-      Impressionist.orm = :mongoid
-      assert_equal Impressionist.orm, :mongoid
+      # sets Imp.orm to active_record in order to
+      # prevent broken tests as they're run randomly
+      imp.orm = :mongoid
+      assert_equal imp.orm, :mongoid
+      imp.orm = :active_record
     end
 
   end
